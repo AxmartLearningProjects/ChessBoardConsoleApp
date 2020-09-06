9244e39795ee1639d24567d60640ac664622666a
Axmart <notaxmart@gmail.com>
2020-09-06 20:44:24 +0200

    Add project files.

---
+++ ChessBoardModel/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    class Board
+    {
+
+        public int Size { get; set; }
+
+        public Cell[,] TheGrid { get; set; }
+
+        public Board(int size)
+        {
+            // The inital board size is defined by size
+            Size = size;
+
+            // create a new 2D array of type cell
+            TheGrid = new Cell[Size, Size];
+
+
+            // fill the 2D array with new Cells, each with unique x and y coordinates
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    TheGrid[i, j] = new Cell(i, j);
+                }
+            }
+
+        }
+
+    }
+}
